f2020bbe7482338c8e4e9406271af042828def33
Abdur-Rahman123 <32038743+Abdur-Rahman123@users.noreply.github.com>
2019-04-23 22:11:00 +0600

    Add files via upload

---
+++ spoj1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int input;
+    while(cin>>input)
+    {
+        if(input!=42)
+        {
+            cout<<input<<endl;
+        }
+    }
+}
